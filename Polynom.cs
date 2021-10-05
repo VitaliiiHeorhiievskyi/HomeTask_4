54c115137e9a61a6fb5a1cf0cd6be0ab2d5cceda
VitaliiiHeorhiievskyi <georgiievsky@gmail.com>
2021-10-06 00:26:24 +0300

    Polynom

--- Polynom.cs
+++ Polynom.cs
@@ -20,6 +20,9 @@ namespace HomeTask_4
             string res = "";
 
             RankAndRealList.Sort();
+
+            //List<(int, double)> rankAndRealList = RankAndRealList;
+
             foreach (var item in RankAndRealList)
             {
                 if (res == "")
@@ -50,34 +53,199 @@ namespace HomeTask_4
 
         public static Polynom Parse(string data)
         {
-            data = data.Replace("X^"," ");
+            data = data.Replace("X^", " ");
             data = data.Replace('+', ' ');
             data = data.Replace(".", ",");
             data = data.Replace("-", " -");
+
             string[] dates = data.Split();
+
             List<(int, double)> rankAndRealList = new List<(int, double)>();
 
+            if (dates.Length % 2 != 0)
+                throw new ArgumentException();
+
+
             int index = 0;
-            (int, double) temp=(0,0);
+
+            (int, double) temp = (0, 0);
 
             foreach (var item in dates)
             {
                 if (index % 2 == 0)
                     temp.Item2 = double.Parse(item);
+
                 if (index % 2 == 1)
                 {
                     temp.Item1 = int.Parse(item);
+
                     rankAndRealList.Add(temp);
                 }
+
                 index++;
             }
             return new Polynom(rankAndRealList);
         }
 
-        public Polynom()
+        public static Polynom operator +(Polynom first, Polynom second)
         {
-            RankAndRealList = new List<(int, double)>();
+            List<(int, double)> RankAndRealList = new List<(int, double)>();
+
+            (int, double) temp = (0, 0);
+
+            bool res = true;
+
+            foreach (var item in first.RankAndRealList)
+            {
+                res = true;
+                foreach (var item2 in second.RankAndRealList)
+                {
+
+                    if (item.Item1 == item2.Item1)
+                    {
+                        temp.Item1 = item.Item1;
+
+                        temp.Item2 = item.Item2 + item2.Item2;
+
+                        RankAndRealList.Add(temp);
+                        res = false;
+                    }
+
+                }
+                if (res)
+                {
+                    RankAndRealList.Add(item);
+                }
+            }
+
+            foreach (var item in second.RankAndRealList)
+            {
+                res = true;
+                foreach (var item2 in first.RankAndRealList)
+                {
+
+                    if (item.Item1 == item2.Item1)
+                    {
+                        res = false;
+                    }
+
+                }
+                if (res)
+                {
+                    RankAndRealList.Add(item);
+                }
+            }
+
+            return new Polynom(RankAndRealList);
         }
 
+        public static Polynom operator -(Polynom first, Polynom second) 
+        {
+            List<(int, double)> RankAndRealList = new List<(int, double)>();
+
+            (int, double) temp = (0, 0);
+
+            bool res = true;
+            foreach (var item in first.RankAndRealList)
+            {
+                res = true;
+                foreach (var item2 in second.RankAndRealList)
+                {
+
+                    if (item.Item1 == item2.Item1)
+                    {
+                        temp.Item1 = item.Item1;
+
+                        temp.Item2 = item.Item2 - item2.Item2;
+
+                        RankAndRealList.Add(temp);
+                        res = false;
+                    }
+
+                }
+                if (res)
+                {
+                    RankAndRealList.Add(item);
+                }
+            }
+
+            foreach (var item in second.RankAndRealList)
+            {
+                res = true;
+                foreach (var item2 in first.RankAndRealList)
+                {
+
+                    if (item.Item1 == item2.Item1)
+                    {
+                        res = false;
+                    }
+
+                }
+                if (res)
+                {
+                    RankAndRealList.Add(item);
+                }
+            }
+
+            return new Polynom(RankAndRealList);
+        }
+        public Polynom(string data)
+        {
+
+            RankAndRealList = new List<(int, double)>(Polynom.Parse(data).RankAndRealList);
+        }
+
+        public double this[int Rank]
+        {
+            get
+            {
+                foreach (var item in RankAndRealList)
+                {
+                    if (item.Item1 == Rank)
+                    {
+                        return item.Item2;
+                    }
+                }
+                throw new ArgumentOutOfRangeException();
+            }
+            set
+            {
+                int index = -1;
+                for (int i = 0; i < RankAndRealList.Count; i++)
+                {
+                    if (RankAndRealList[i].Item1 == Rank)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                (int, double) temp;
+                if (value == 0 && index == -1)
+                {
+
+                }
+                else if (index == -1 && value != 0)
+                {
+                    temp.Item1 = Rank;
+
+                    temp.Item2 = value;
+
+                    RankAndRealList.Add(temp);
+                }
+                else if (index != -1 && value == 0)
+                {
+                    RankAndRealList.RemoveAt(index);
+                }
+                else if (index != -1 && value != 0)
+                {
+                    RankAndRealList[index] = (Rank, value);
+                }
+            }
+        }
+
+        public string PrintMenu()
+        {
+            return "\n[1]Print Polynom 1\t[2]Print Polynom 2\n[3]Add\t\t\t[4]Subtraction\n[5]Change coefficient\t[0] Exit\n";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,13 +7,7 @@ namespace HomeTask_4
     {
         static void Main(string[] args)
         {
-            Polynom polynom = new Polynom();
-
-            polynom=Polynom.Parse("2X^3-3X^2+2.3X^4");
-
-            Console.WriteLine(polynom);
-
-            Console.WriteLine(polynom);
+            InputFromConsole.Start();
         }
     }
 }
